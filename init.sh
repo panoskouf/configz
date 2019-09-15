d7b92d2dfa8e00e90ad878c995a11a8805e8540d
panoskouf <panoskouf@outlook.com>
2019-09-15 08:47:24 +0300

    updates

--- init.sh
+++ init.sh
@@ -3,3 +3,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo '(load-file "'$DIR/linked/init.el'")' > ~/.emacs.d/init.el
 echo 'source '$DIR/linked/fish/init.fish > ~/.config/omf/init.fish
 echo 'source '$DIR/linked/.bashrc > ~/.bashrc
+# link vscode
+cd ~/.config/Code/User
+mkdir snippets
+rm -r keybindings.json settings.json snippets/*
+cd $DIR
+cd linked/vscode
+ln keybindings.json ~/.config/Code/User/keybindings.json
+ln settings.json ~/.config/Code/User/settings.json
+ln snippets/javascript.json ~/.config/Code/User/snippets/javascript.json
+ln snippets/javascriptreact.json ~/.config/Code/User/snippets/javascriptreact.json
