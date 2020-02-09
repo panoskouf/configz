e9d50928aa3aad27ff1d84d3656d6db4456ad2c1
panoskouf <panoskouf@outlook.com>
2020-02-09 18:46:37 +0200

    start dropbox on startup

--- scripts/runOnBoot.sh
+++ scripts/runOnBoot.sh
@@ -2,9 +2,11 @@
 
 # add /usr/local/scripts/runOnBoot.sh to settings -> applications -> startup
 
+echo "" >  /home/panoskouf/startup_log.txt
+
+/usr/local/scripts/dropbox.py start
+
+echo "`/usr/local/scripts/dropbox.py status`" > /home/panoskouf/startup_log.txt
 
 # listen for layout change
-/usr/local/scripts/switch_layout.py &
-
-# detach process from shell
-disown
+/usr/local/scripts/switch_layout.py
