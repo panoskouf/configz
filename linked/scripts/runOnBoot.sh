36885586156b2ec4b4d120d1f5d694e2ce3c9c8d
panoskouf <panoskouf@outlook.com>
2020-02-11 21:42:30 +0200

    no need to log

--- linked/scripts/runOnBoot.sh
+++ linked/scripts/runOnBoot.sh
@@ -2,11 +2,14 @@
 
 # add /usr/local/scripts/runOnBoot.sh to settings -> applications -> startup
 
-echo "" >  /home/panoskouf/startup_log.txt
+
+
+
 
 /usr/local/scripts/dropbox.py start
 
-echo "`/usr/local/scripts/dropbox.py status`" > /home/panoskouf/startup_log.txt
+
+
 
 # listen for layout change
 /usr/local/scripts/switch_layout.py
